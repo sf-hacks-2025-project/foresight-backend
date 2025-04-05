e645663ec687315bfadabd6d76af935e2de98e5f
Dmitriy Gamolya <109139706+DrowsyTM@users.noreply.github.com>
2025-04-05 16:48:09 -0700

    changed tts to return .mp3 file directly

--- routes/tts.py
+++ routes/tts.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 from fastapi import APIRouter, status
 from modules import eleven_labs
 from elevenlabs import AsyncElevenLabs, VoiceSettings
@@ -10,7 +12,7 @@ router = APIRouter()
 async def tts(text: str):
     client: AsyncElevenLabs = await eleven_labs.get_eleven_client()
     response = client.text_to_speech.convert(
-        voice_id=os.getenv('ELEVEN_LABS_VOICE_ID'),
+        voice_id="9BWtsMINqrJLrRacOk9x", # We can have a dropdown to change this
         output_format="mp3_22050_32",
         text=text,
         model_id="eleven_flash_v2_5",
@@ -19,13 +21,19 @@ async def tts(text: str):
             similarity_boost=1.0,
             style=0.0,
             use_speaker_boost=True,
-            speed=1.05,
+            speed=1.0,
         ),
     )
 
-    async def audio_streamer():
-        async for chunk in response:
-            if chunk:
-                yield chunk
+    audio_data = BytesIO()
+    async for chunk in response:
+        if chunk:
+            audio_data.write(chunk)
 
-    return StreamingResponse(audio_streamer(), media_type="audio/mpeg")
+    audio_data.seek(0)  # rewind to start
+
+    return StreamingResponse(
+        audio_data,
+        media_type="audio/mpeg",
+        headers={"Content-Disposition": "attachment; filename=output.mp3"}
+    )
