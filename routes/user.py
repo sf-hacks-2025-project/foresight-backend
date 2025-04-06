cdabc5b1d97fb02522630151c0044a4ed6a55609
DZ <ansontan635@gmail.com>
2025-04-05 22:16:48 -0700

    Backend user id log

--- routes/user.py
+++ routes/user.py
@@ -8,4 +8,6 @@ router = APIRouter()
 # Generates a user id using uuidv4 and returns it
 @router.get("/user/create", status_code=status.HTTP_200_OK)
 async def register():
-    return {"user_id": str(uuid.uuid4())}
+    user_id = str(uuid.uuid4())
+    print("Generated user ID: " + user_id)
+    return {"user_id": user_id}
